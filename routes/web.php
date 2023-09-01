73bb72b68ba17f58a743b8513f8c1bba453a8df8
daniyofaf <daniyoman1993@gmail.com>
2023-09-01 14:34:01 +0300

    search is added on route

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/search/', 'PostsController@search')->name('search');
+Route::get('/search/', 'PostsController@search')->name('search');   //search
